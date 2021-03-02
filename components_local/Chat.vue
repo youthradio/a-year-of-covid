f392d6731da5eb1d799c30efea39a93f9a994447
Radames Ajna <radamajna@gmail.com>
2021-03-01 22:54:54 -0800

    break chat into component

---
+++ components_local/Chat.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ph3">
+    <h3 class="tc">Chat</h3>
+    <div>
+      <template v-for="chat in content">
+        <div :key="chat.text">
+          <div>
+            <span class="blue"> {{ chat.from }}</span> to
+            <span class="blue">{{ chat.to }}</span>
+          </div>
+          <div
+            class="br4 pa3 bg-lightest-blue reset-p"
+            v-html="chat.text"
+          ></div>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
