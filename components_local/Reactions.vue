97b6751582293e0185aa2602b5ac4071548aba14
shagia <usenet@shagia.me>
2021-03-04 14:44:33 -0800

    Define space for emoji spawning

--- components_local/Reactions.vue
+++ components_local/Reactions.vue
@@ -1,14 +1,18 @@
 <template>
-  <div
-    class="f6 br4 pa2 bg-near-black light-silver mw5 shadow-2 container flex"
-    @focusout="onFocus"
-  >
-    <span class="f5 db">👏</span>
-    <span class="f5 db">👏</span>
-    <span class="f5 db">👏</span>
-    <span class="f5 db">👏</span>
-    <span class="f5 db">👏</span>
-    <span class="f5 db">👏</span>
+  <div class="">
+    <div
+      class="f6 br4 pa2 bg-near-black light-silver mw5 shadow-2 container flex"
+      @focusout="onFocus"
+    >
+      <span class="f5 db">👏</span>
+      <span class="f5 db">👏</span>
+      <span class="f5 db">👏</span>
+      <span class="f5 db">👏</span>
+      <span class="f5 db">👏</span>
+      <span class="f5 db">👏</span>
+    </div>
+    <!-- Unfinished note, because tabs are defined in limited spaces, it's hard to make a space for the emojis that sit on a responive corner of the screen, so I'm more doing hacky positioning to simply getting it working for now -->
+    <div class="reaction-space absolute w-100">Test</div>
   </div>
 </template>
 
@@ -36,5 +40,8 @@ export default {
 <style lang="scss" scoped>
 .container:focus {
   outline: 0.1rem dashed rgba(lightgray, 0.1);
+}
+.reaction-space {
+  left: 90%;
 }
 </style>
