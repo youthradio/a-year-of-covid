770e235c5593abf64c2883ff2b05118bc463b515
shagia <usenet@shagia.me>
2021-03-04 15:39:11 -0800

    Bare basic form of particle creation, only forms an element named 'emoji'. Not correct

--- components_local/Reactions.vue
+++ components_local/Reactions.vue
@@ -4,15 +4,17 @@
       class="f6 br4 pa2 bg-near-black light-silver mw5 shadow-2 container flex"
       @focusout="onFocus"
     >
-      <span class="f5 db">👏</span>
-      <span class="f5 db">👏</span>
-      <span class="f5 db">👏</span>
-      <span class="f5 db">👏</span>
-      <span class="f5 db">👏</span>
-      <span class="f5 db">👏</span>
+      <span class="f5 db" @click="test('clap emoji')">👏</span>
+      <span class="f5 db" @click="test('star emoji')">👏</span>
+      <span class="f5 db" @click="test('washed emoji')">👏</span>
+      <span class="f5 db" @click="test('cute emoji')">👏</span>
+      <span class="f5 db" @click="test('emo emoji')">👏</span>
+      <span class="f5 db" @click="test('cryin emoji')">👏</span>
     </div>
     <!-- Unfinished note, because tabs are defined in limited spaces, it's hard to make a space for the emojis that sit on a responive corner of the screen, so I'm more doing hacky positioning to simply getting it working for now -->
-    <div class="reaction-space absolute w-100">Test</div>
+    <div ref="emojiSpace" class="reaction-space absolute w-100">
+      This is some text to test the spacing limits and appearance
+    </div>
   </div>
 </template>
 
@@ -28,11 +30,20 @@ export default {
     this.$el.focus()
   },
   methods: {
+    test(emoji) {
+      console.log('Recieved a click, emoji is ' + emoji)
+      console.log(this.$refs.emojiSpace)
+      const createEmoji = document.createElement('emoji')
+      this.$refs.emojiSpace.appendChild(createEmoji)
+    },
     onFocus(e) {
       if (e.type === 'focusout') {
         this.$store.dispatch('toggleUIState', 'reactions')
       }
     },
+  },
+  render(createElement) {
+    return createElement('h1', 'Hello world')
   },
 }
 </script>
@@ -42,6 +53,6 @@ export default {
   outline: 0.1rem dashed rgba(lightgray, 0.1);
 }
 .reaction-space {
-  left: 90%;
+  left: 200%;
 }
 </style>
