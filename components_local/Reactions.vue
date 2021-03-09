18a6a6de700dfbd903c29be72ee4eacdb0242f4e
shagia <usenet@shagia.me>
2021-03-09 12:59:37 -0800

    Make emojis respective to props passed

--- components_local/Reactions.vue
+++ components_local/Reactions.vue
@@ -4,12 +4,13 @@
       class="f6 br4 pa2 bg-near-black light-silver mw5 shadow-2 container flex"
       @focusout="onFocus"
     >
-      <span class="f5 db" @click="test('clap emoji')">👏</span>
-      <span class="f5 db" @click="test('star emoji')">👏</span>
-      <span class="f5 db" @click="test('washed emoji')">👏</span>
-      <span class="f5 db" @click="test('cute emoji')">👏</span>
-      <span class="f5 db" @click="test('emo emoji')">👏</span>
-      <span class="f5 db" @click="test('cryin emoji')">👏</span>
+      <span
+        v-for="(emoji, index) in content"
+        :key="index"
+        class="f5 db"
+        @click="test(emoji)"
+        >{{ emoji }}</span
+      >
     </div>
     <!-- Unfinished note, because tabs are defined in limited spaces, it's hard to make a space for the emojis that sit on a responive corner of the screen, so I'm more doing hacky positioning to simply getting it working for now -->
     <div ref="emojiSpace" class="reaction-space absolute w-100"></div>
@@ -18,7 +19,12 @@
 
 <script>
 export default {
-  props: {},
+  props: {
+    content: {
+      type: Array,
+      required: true,
+    },
+  },
   mounted() {
     this.$el.focus()
   },
@@ -33,7 +39,7 @@ export default {
       console.log(dice)
       // create the element itself
       const createEle = document.createElement('span')
-      const createEmoji = document.createTextNode('😋')
+      const createEmoji = document.createTextNode(emoji)
       createEle.style.fontSize = `${randoSize}px`
       createEle.classList.add('emoji')
       createEle.appendChild(createEmoji)
@@ -50,6 +56,7 @@ export default {
         createEle.style.top = `${randoSize}px`
       }
 
+      // used as the main animation
       const leadAnimation = createEle.animate(
         [
           {
@@ -59,15 +66,17 @@ export default {
             transform: 'translate(0px,-300px)',
             opacity: 0,
             textSize: `${randoSize}px`,
+            // maybe its worth linking opacity to its own animation so it wouldnt apply to the easing
           },
         ],
         {
-          duration: 3000,
+          duration: 1500,
           easing: 'cubic-bezier(0, .96, .46, 0.07)',
-          // Delay every particle with a random value from 0ms to 200ms
+          // delay emoji rendering from 0 to 200 milliseconds
           delay: Math.random() * 200,
         }
       )
+      // wiggly
       createEle.animate(
         [
           {
@@ -87,15 +96,10 @@ export default {
           easing: 'ease-in-out',
         }
       )
-
+      // when the 1.5 seconds are up, go away permanently
       leadAnimation.onfinish = () => {
         createEle.remove()
       }
-
-      // Replace with the animation api's onfinish function when animations are done
-      setTimeout(function () {
-        // createEle.remove()
-      }, 3000)
     },
     onFocus(e) {
       if (e.type === 'focusout') {
@@ -106,8 +110,8 @@ export default {
 }
 </script>
 
-// does the style need to be scoped? I can't target new elements for some
-reason...
+// does the style need to be scoped? I can't target new elements for some reason
+when scoped...
 
 <style lang="scss">
 .container:focus {
@@ -115,9 +119,10 @@ reason...
 }
 .reaction-space {
   left: 200%;
-  * {
+  .emoji {
     position: absolute;
     opacity: 0;
+    pointer-events: none;
   }
 }
 </style>
