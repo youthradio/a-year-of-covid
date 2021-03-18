109466ee2441f9261c577ca598887762d4f4bb76
radames <radames@users.noreply.github.com>
2021-03-18 05:50:14 +0000

    deploy: e995f633a7faaadb7441133b5a78f617370a5bf1

---
+++ _nuxt/static/1616046612/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002Fzoomiversary\u002F_nuxt\u002Fstatic\u002F1616046612",layout:"default",error:b,state:{UIState:{chat:c,credits:a,reactions:a,contributors:a,more:a}},serverRendered:c,routePath:"\u002F",config:{app:{basePath:"\u002Fzoomiversary\u002F",assetsPath:"\u002Fzoomiversary\u002F_nuxt\u002F",cdnURL:b}}}}(false,null,true));
